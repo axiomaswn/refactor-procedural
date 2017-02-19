9948303402b523de41d94c1daffdf822809d826b
YOMA <email": null>
2017-02-19 13:38:15 +0700

    selesai mvc

--- server/app.js
+++ server/app.js
@@ -1,13 +1,25 @@
-$(document).ready(function(){
-  $('#roller button.add').on('click', function(){
+let model = {
+  random: function(k,die){
+    var value =  Math.floor((Math.random()*6)+1)
+    $(die).text(value)
+  }
+}
+
+let view = {
+  roll: function() {
+    $('#roller button.add').on('click', controller.add)
+    $('#roller button.roll').on('click', controller.dice)
+  }
+}
+
+let controller = {
+  add: function() {
     console.log("WAT")
     $('.dice').append('<div class="die">0</div>')
-  })
-  $('#roller button.roll').on('click',function(){
-    $('.die').each(function(k,die){
-      var value = Math.floor((Math.random()*6)+1)
-      $(die).text(value)
-    })
-  })
-})
-$(document).ready()
+  },
+
+  dice: function() {
+    $('.die').each(model.random)
+  }
+}
+$(document).ready(view.roll)
